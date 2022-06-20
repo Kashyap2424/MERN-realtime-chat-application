51b29b9a534a8639d20c4e66a3c0c20a3328ce54
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-20 15:05:08 +0530

    Write event for server side send message

--- server/app.js
+++ server/app.js
@@ -24,6 +24,10 @@ io.on("connection", (socket) => {
     console.log(`User with ID: ${socket.id}, Joined room: ${data}`);
   });
 
+  socket.on("send_message", (data) => {
+    console.log(data);
+  });
+
   socket.on("disconnect", () => {
     console.log("User Disconnected...", socket.id);
   });
