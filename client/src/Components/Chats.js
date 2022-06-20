ed1acdfbca4e91edf578df1fe73aa965c594de99
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-20 14:52:29 +0530

    Complete chats component and emit the one socket.io event

---
+++ client/src/Components/Chats.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Chats = ({ socket, userName, roomId }) => {
+  const [currentMessage, setCurrentMessage] = useState();
+
+  const sendMessageHandler = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: roomId,
+        authore: userName,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+    }
+  };
+  return (
+    <div>
+      <div className="chat-header">
+        <p>Live chat</p>
+      </div>
+      <div className="chat-body"></div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Message"
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+          }}
+        />
+        <button onClick={sendMessageHandler()}>&#9658;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
